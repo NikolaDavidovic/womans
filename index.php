7992ee2ca1ef6812213f3424ea8ff76a6c066267
NikolaDavidovic <nikola.davidovic92@gmail.com>
2016-10-24 21:25:10 +0200

    changes

--- index.php
+++ index.php
@@ -1,4 +1,37 @@
-<?php echo "Hello madafaka"; 
-echo "konflikt";
-echo "konflikt222";
-?>
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+        <link rel="stylesheet" type="text/css" href="css/style.css">
+        <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+        <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+    </head>
+    <body>
+        <div align="center" class="embed-responsive embed-responsive-16by9">
+            <!--                <video autoplay loop class="embed-responsive-item">
+                                <source src=video/video.mp4 type=video/mp4>
+                            </video>-->
+        </div>
+
+        <div class="pages"> 
+            <a href="speakers.php">
+                <img src="img/speakers.jpg" alt="speakers"/>
+            </a>
+        </div>
+
+        <div class="footer">
+            <b> smarteksystems.ca </b>
+            <div class="col-md-6"> Powered by <b> smartek </b> </div>
+        </div>
+
+        <script type="text/javascript" src="js/bootstrap.js"></script>
+        <script type="text/javascript" src="js/bootstrap.min.js"></script>
+        <script type="text/javascript" src="js/jquery.js"></script>
+    </body>
+</html>
