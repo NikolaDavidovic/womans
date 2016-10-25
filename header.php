71612b608ce9e212b1be97d243e503c59afa7615
Milos Zivanovic <z.milos93@yahoo.com>
2016-10-25 17:18:09 +0200

    working on map.php

--- header.php
+++ header.php
@@ -2,7 +2,7 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <meta charset="UTF-8">
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/> 
         <title></title>
         <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
         <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
--- js/maphilight-trigger.js
+++ js/maphilight-trigger.js
@@ -1,14 +1,14 @@
 $(function () {
     $('img.map').maphilight({stroke: false, fillColor: "99DCF2", fillOpacity: "0.75"});
 
-    $('ul.area-links li').click(function () {
-        $('ul.area-links li').css('color', 'black');
+    $('ul.area-links li span').click(function () {
+        $('ul.area-links li span').css('color', 'black');
         $(this).css('color', '#5e2e85');
         $('html, body').animate({
             scrollTop: $("#map-img").offset().top
         }, 2000);
-        $('.map-wrapper h4').text($(this).children().text());
+        $('.map-wrapper h4').text($(this).text());
         $('[id^="area"]').mouseout();
-        $($(this).data('area')).mouseover();
+        $($(this).parent().data('area')).mouseover();
     })
 });
