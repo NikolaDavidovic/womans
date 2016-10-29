c1eec4cb45103693fdca9a5fb6b62c7b3e4888c9
NikolaDavidovic <nikola.davidovic92@gmail.com>
2016-10-30 01:16:53 +0200

    continued working on schedule

--- schedule.php
+++ schedule.php
@@ -164,153 +164,118 @@ Diamond Group </p>
         <p class="stage text-center"> GLAM RUNWAY </p>
 
     <div class="col-md-4"> <p class="stage text-center"> FRIDAY </p>
-        <p class="time"> 12:00 p.m. </p>
-        <p class="name"> Taking the Time for Tea </p>
-        <p class="author"> Yvonne Robertson </p>
-        <p class="author"> SAHARA TEA </p>
+        <p class="time"> 12:30 p.m. </p>
+        <p class="name"> Accessories For
+Your Lifestyle </p>
+        <p class="author"> Franca Fasulo </p>
+        <p class="author"> SAYAN Inc. </p>
         <br/>
 
         <p class="time"> 1:15 p.m. </p>
-        <p class="name"> Smoothies - How To Make 'Em </p>
-        <p class="author"> Vicenza Mussico </p>
-        <p class="author"> ROYAL DATES </p>
+        <p class="name"> Designer Umbrella Show
+Fashion Show </p>
+        <p class="author"> 
+La Bella Umbrella </p>
         <br/>
 
         <p class="time"> 2:00 p.m. </p>
-        <p class="name"> 3 Meals in 30 Minutes </p>
-        <p class="author"> Anne Elsbury and Mary Marino </p>
-        <p class="author">  LA DEE DA GOURMET SAUCES</p>
+        <p class="name"> C.C By Corinne Campenio </p>
+        <p class="author"> Jessica Hoffman </p>
+        <br/>
+
+        <p class="time"> 2:45 p.m. </p>
+        <p class="name"> The Unexpected Art to Wear
+Fashion Show </p>
+        <p class="author"> Heather Chapplain </p>
         <br/>
 
         <p class="time"> 3:30 p.m. </p>
-        <p class="name"> SPONSORED BY Nature's Path </p>
-        <br/>
-
-        <p class="time"> 4:15 p.m. </p>
-        <p class="name"> Gluten Freedom: The Easy Way To Healthy Eating without Sacrificing Taste </p>
-        <p class="author"> Executive Chef Jeffrey Willfong </p>
-        <p class="author"> STE. ANNE'S SPA DESTINATION </p>
+        <p class="name"> Breast Rest: Make Wearing
+a Bra Comfortable </p>
+        <p class="author"> Jody Harbou </p>
+        <p class="author"> Breast Rest </p>
         <br/>
 
         <p class="time"> 5:00 p.m. </p>
-        <p class="name"> The Great Gorp Project </p>
-        <p class="author"> Colleen Dyck </p>
-        <p class="author"> GORP ENERGY BARS </p>
+        <p class="name"> The Ultimate Brow Tutorial </p>
+        <p class="author"> Illona Alfaro & Jessica Lloyd </p>
+        <p class="author"> Lloyd Alfaro </p>
         <br/>
 
-        <p class="time"> 5:45 p.m. </p>
-        <p class="name"> The Kirkwood Diamond Group </p>
-        <br/>
-
-        <p class="time"> 6:30 p.m. </p>
-        <p class="name"> Make your own Fermented Foods </p>
-        <p class="author"> Rani Glick </p>
-        <p class="author"> THE WELLNESS WAGON </p>
         <br/>
     </div>
 
     <div class="col-md-4 day"> <p class="stage text-center"> SATURDAY </p> 
-        <p class="time"> 10:30 a.m. </p>
-        <p class="name"> Healthy Can
-            Taste SO Good! </p>
-        <p class="author">Brenda Ross and
-            Madeliene Garet </p>
-        <p class="author"> LOW CARB CANADA </p>
-        <br/>
 
         <p class="time"> 11:15 a.m. </p>
-        <p class="name"> HMeet Miscela With Chef D!
-            Introducing Your New
-            Favourite Secret Ingredient </p>
-        <p class="author">Chef Darryl Fletcher </p>
-        <p class="author"> DERLEA BRAND FOODS </p>
-        <br/>
-
-        <p class="time"> 12:00 p.m. </p>
-        <p class="name"> Live Baci Chocolate Theatre </p>
-        <p class="author">Chef Christian Pritchard </p>
-        <p class="author"> AURORA IMPORTS </p>
+        <p class="name"> Nature's Own </p>
+        <p class="author">Jordane Cosmetic </p>
         <br/>
 
         <p class="time"> 12:45 p.m. </p>
-        <p class="name"> Lunch/Dinner Prep - How To
-            Use Leftover Chicken! </p>
-        <p class="author">Kelly Atyeo-Fick </p>
-        <p class="author"> CANADIAN CHICKEN
-            FARMERS OF CANADA </p>
+        <p class="name"> Retail Therapy
+Fashion Show </p>
+        <p class="author">Daniela Schmidt </p>
         <br/>
 
         <p class="time"> 1:30 p.m. </p>
-        <p class="name"> SPONSORED BY
-            Nature's Path </p>
+        <p class="name"> The Ultimate Brow Tutorial </p>
+        <p class="author">Illona Alfaro & Jessica Lloyd </p>
+        <p class="author"> Lloyd Alfaro </p>
         <br/>
 
         <p class="time"> 2:15 p.m. </p>
-        <p class="name"> The Kirkwood
-            Diamond Group </p>
+        <p class="name"> Tofara - An Afro
+Chic Experience </p>
+        <p class="author"> Eriver Muwanguzi </p>
+        <p class="author"> Tofara </p>
         <br/>
 
         <p class="time"> 3:00 p.m. </p>
-        <p class="name"> Ayurveda and Ways To
-            Find Your Balance </p>
-        <p class="author">Debra Joy Eklove,
-            Ayurvedic Practitione </p>
-        <p class="author"> TETLEY CANADA </p>
+        <p class="name"> Comfort with Style </p>
+        <p class="author">Esther Isman, </p>
+        <p class="author"> ESH by Estee </p>
         <br/>
 
         <p class="time"> 3:45 p.m. </p>
-        <p class="name"> Topic TBD </p>
-        <p class="author">Kiran Kitaria & Rishi Kataria, </p>
-        <p class="author"> KFI FOODS </p>
+        <p class="name"> C.C By Corinne Campenio </p>
+        <p class="author">Jessica Hoffman </p>
         <br/>
         
         <p class="time"> 4:30 p.m. </p>
-        <p class="name"> Everyone Deserves
-Quality Food </p>
-        <p class="author">Jihad Sarout </p>
-        <p class="author"> FONTAINE SANTE </p>
+        <p class="name"> Breast Rest: Make Wearing
+a Bra Comfortable </p>
+        <p class="author">Jody Harbour </p>
+        <p class="author"> Breast Rest </p>
         <br/>
         
-        <p class="time"> 5:15 p.m. </p>
-        <p class="name"> Quick and Easy Meals </p>
-        <p class="author">Rani Glick </p>
-        <p class="author"> THE WELLNESS WAGON </p>
-        <br/>
     </div>
+        
     <div class="col-md-4 day"> <p class="stage text-center"> SUNDAY </p> 
         <p class="time"> 10:30 a.m. </p>
-        <p class="name"> Allergy Free Cooking </p>
-        <p class="author">Jules Lieff </p>
-        <p class="author"> ORGANIC GARAGE </p>
-        <br/>
-        
-        <p class="time"> 11:15 a.m. </p>
-        <p class="name"> Ladies Brunch with
-Canadian Chicken </p>
-        <p class="author">Kelly Atyeo-Fick </p>
-        <p class="author"> FARMERS OF CANADA </p>
+        <p class="name"> Designer Umbrella Show
+Fashion Show </p>
+        <p class="author">La Bella Umbrella </p>
         <br/>
         
         <p class="time"> 12:00 p.m. </p>
-        <p class="name"> The Kirkwood
-Diamond Group </p>
+        <p class="name"> CC by Corrine - Fall
+Accessory Collection </p>
+        <p class="author">Corinne Campenio </p>
         <br/>
         
         <p class="time"> 12:45 p.m. </p>
-        <p class="name"> SPONSORED BY
-            Nature's Path </p>
+        <p class="name"> Breast Rest: Make Wearing
+a Bra Comfortable </p>
+        <p class="author">Jody Harbour </p>
+        <p class="author">Breast Rest </p>
         <br/>
         
         <p class="time"> 3:00 p.m. </p>
-        <p class="name"> Unlock Your Inner Chef </p>
-        <p class="author">Ms. Charlotte Lemaire </p>
-        <p class="author"> MAISON ORPHÉE </p>
-        <br/>
-        
-        <p class="time"> 3:45 p.m. </p>
-        <p class="name"> Good Food for Good Health </p>
-        <p class="author">Rani Glick </p>
-        <p class="author"> THE WELLNESS WAGON </p>
+        <p class="name"> Accessories For
+Your Lifestyle </p>
+        <p class="author">Franca Fasulo </p>
+        <p class="author">SAYAN Inc </p>
         <br/>
     </div>
     
